75322ed329ca2210f1929b8a9683494f2dedc3dc
LautaroLopez2 <lautalopez2448@gmail.com>
2022-10-12 21:00:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tp7_ejr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int num = 0;
+            int resultado =0;
+            string entrada;
+
+            Console.WriteLine("Escriba un numero al que le quiera sacar la factorial");
+            entrada = Console.ReadLine();
+            num = Convert.ToInt32(entrada);
+
+            
+
+            resultado = factorial(num);
+            Console.WriteLine("el factorila del numero {0} es {1}", num, resultado);
+
+        }
+        static int factorial( int num)
+        {
+            int acumulador = 0;
+            for (int i = 1; i < num; i++)
+            {
+                acumulador = num * i;
+            }
+            return acumulador;
+        }
+    }
+
+}
